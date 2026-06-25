05b1272f1a644c7c8967fb6eb7d2ce81c47f73f6
Weslley Gatti <weslleygatti1@hotmail.com>
2026-06-25 02:28:38 -0300

    finalizando desafio

---
+++ src/main/Main.java
@@ -0,0 +1,41 @@
+package main;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.Scanner;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+public class Main {
+    public static void main(String[] args) throws IOException, InterruptedException {
+        System.out.println("Digite o CEP que voce quer procurar:");
+        var scan = new Scanner(System.in);
+        String cep = scan.nextLine();
+
+        String url = "https://viacep.com.br/ws/" + cep + "/json";
+        try {
+            HttpClient client = HttpClient.newHttpClient();
+            HttpRequest request = HttpRequest.newBuilder().uri(URI.create(url)).build();
+            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+            Gson gson = new GsonBuilder().setPrettyPrinting().create();
+            String json = gson.toJson(response.body());
+            System.out.println(json);
+            var escritor = new FileWriter("endereco.json");
+            escritor.write(json);
+            escritor.close();
+        } catch (IOException e) {
+            e.getMessage();
+        }
+        // scan.close();
+
+    }
+}
+
+// api: viacep
+// menu para interacao com o usuario
+// geracao de um json com os dados do endereço
